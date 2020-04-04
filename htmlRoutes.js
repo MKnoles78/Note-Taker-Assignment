4db1a249611c767b597e1b4be015435910b35c54
MKnoles <mknoles78@gmail.com>
2020-04-04 00:58:47 -0400

    added middleware and get api notes from db.json

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,15 +1,33 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 
-module.exports = function (app){
 
-// GET `/notes` - Should return the `notes.html` file.
-app.get("/notes", function(req, res){
-   res.sendFile(path.join(__dirname, "..public/notes.html")); 
-});
+const app = express();
 
-// * GET `*` - Should return the `index.html` file
-app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// gets the root folder 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
 })
-};
+
+// gets the notes folder 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+
+// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) throw err;
+    })
+})
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log("server is listening on " + PORT))
